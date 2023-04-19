ddf69a948d19ad3781c316ef07721fd771e4321f
morganjeanna <mjlucas@ucsd.edu>
2023-04-19 15:53:50 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace1() {
+    int[] input1 = {1, 3, 4, 5, 6, 7, };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{7, 6, 5, 4, 3, 2, 1}, input1);
+  }
+
+  @Test
+  public void testReversed2(){
+    int[] input1 = {0, 0, 2};
+    assertArrayEquals(new int[]{2, 0, 0}, ArrayExamples.reversed(input1));
+  }
+  
 }
